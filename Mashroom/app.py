aa548948549b48a8df06f325a0dc1223dcc96dba
Siddhiraj Ramesh Kolwankar <siddhirajk77@gmail.com>
2021-10-11 22:07:41 +0530

    updated file
    updated file

--- Mashroom/app.py
+++ Mashroom/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as  np
 import pandas  as pd
-import pickle
+import pickle 
 from sklearn.tree import DecisionTreeClassifier
 from Logger.logger import Logs
 from flask_mysqldb import MySQL
